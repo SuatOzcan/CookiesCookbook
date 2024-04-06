e2ff0d9215201ae3db2979e2288fd7c5fb81c72b
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-04-06 12:50:12 +0300

    Allow the selection of writing to and reading from text or JSON files.

--- CookiesCookbook/CookiesCookbook/Program.cs
+++ CookiesCookbook/CookiesCookbook/Program.cs
@@ -4,16 +4,46 @@
     {
         static void Main(string[] args)
         {
+            FileFormat fileFormat = FileFormat.Json;
+            IStringsRepository stringsRepository;
+            string filePath = "recipes";
+
+            switch (fileFormat)
+            {
+                case FileFormat.Json:
+                    stringsRepository = new StringsJSONRepository();
+                    filePath += ".json";
+                    break;
+                case FileFormat.Txt:
+                    stringsRepository = new StringsTextualRepository();
+                    filePath += ".txt";
+                    break;
+                default:
+                    stringsRepository = new StringsTextualRepository();
+                    filePath = ".txt";
+                    break;
+            }
+
             IngredientsRegister ingredientsRegister = new IngredientsRegister();
 
-            CookiesRecipesApp cookiesRecipesApp = new CookiesRecipesApp(new RecipesRepository(
-                                                                          new StringsJSONRepository(),
-                                                                          ingredientsRegister),
-                                                                        new RecipesConsoleUserInteraction(
-                                                                            ingredientsRegister
-                                                                            )
+            CookiesRecipesApp cookiesRecipesApp = new CookiesRecipesApp(
+                                                                        new RecipesRepository
+                                                                                  (
+                                                                                  stringsRepository,
+                                                                                  ingredientsRegister
+                                                                                  ),
+                                                                        new RecipesConsoleUserInteraction
+                                                                                  (
+                                                                                  ingredientsRegister
+                                                                                  )
                                                                         );
-            cookiesRecipesApp.Run("recipes.json");
+            cookiesRecipesApp.Run(filePath);
+        }
+
+        public enum FileFormat
+        {
+            Json,
+            Txt
         }
     }
 }
